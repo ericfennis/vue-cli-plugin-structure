5157024a0aeafd35fd443eadfe133491f1bf0964
Eric Fennis <eric.fennis@mediamonks.com>
2018-12-28 16:53:19 +0100

    Add store modules description

--- generator/template/src/store/modules/index.js
+++ generator/template/src/store/modules/index.js
@@ -1,5 +1,6 @@
-/* eslint-disable import/prefer-default-export, import, prettier/prettier */
-export { default as app } from './app';
-
+// STORE MODULES IMPORTER
 // Add new modules by adding:
 // + export { default as {MyStoreModule} } from './{MyStoreModule}';
+//
+/* eslint-disable import/prefer-default-export, import, prettier/prettier */
+export { default as app } from './app';
